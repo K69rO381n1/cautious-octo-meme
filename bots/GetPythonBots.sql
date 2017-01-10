c2ab231618da1b2ba2cff797bc35196daa1f5281
Kfir Ochana <kfir.ochana@gmail.com>
2017-01-10 14:00:06 +0300

    Add SQL query for getting all the challenge bots. Another for getting the latest group's bot will be available  soon.

---
+++ bots/GetPythonBots.sql
@@ -0,0 +1,17 @@
+﻿DO $$
+DECLARE bot RECORD;
+
+BEGIN
+	FOR bot IN (
+			SELECT 
+			  code, filename
+			FROM 
+			  public.challenge_bots
+			WHERE
+			  filename LIKE '%.py'
+		   )
+	LOOP
+		RAISE NOTICE  'Saving % into file', bot.filename;
+		PERFORM pg_catalog.pg_file_write(bot.filename, (select encode(bot.code::bytea, 'escape')), false);
+	END LOOP;
+END$$;
