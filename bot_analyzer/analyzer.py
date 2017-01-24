c2198945f79c67bc4850ca0c471f9a9bb775e568
Kfir Ochana <kfir.ochana@gmail.com>
2017-01-24 12:16:06 +0200

    Copy pythonRunner.py from the game's libs. Make analyzer inject function more generic in order to support all of the bot's programming languages in the future. Add initial form to the GameWrapper class.

--- bot_analyzer/analyzer.py
+++ bot_analyzer/analyzer.py
@@ -1,29 +1,60 @@
 # module for analyzing existing bot.
-import re
+from os.path import splitext
+
 # default template file
 default_file = 'template.py'
 
+TAB = '    '
+
+PYTHON_EXT = '.py'
+JAVA_EXT = '.java'
+C_SHARP_EXT = '.cs'
+
+PY_DO_TURN_STATEMENT = u'def do_turn\(.+\):'
+PY_GAME_WRAPPER_PATH = u'\bot_analyzer\game_wrapper.py'
+PY_GAME_OBJECT_WRAPPING = TAB + u'game = GameWrapper(game)'
+
 
 def load(filename):
-    '''
+    """
     loads file named filename to a string
-    '''
+    """
     with open(filename) as f:
         return '\n'.join(f)
 
 
 def save(string, filename):
-    '''
+    """
     saves string to file named filename
-    '''
+    """
     with open(filename, 'w') as f:
         f.write(string)
 
-def manipulate(string):
-    place = re.search(u'def do_turn\(.+\):', string).end(0)
-    print place
+
+def inject(code, search_string, inject_string, place_before_or_after):
+    return code.replace(search_string,
+                        search_string + inject_string if
+                        place_before_or_after else
+                        inject_string + search_string, count=1)
+
+
+def inject_adapter(bot_file_path):
+    code = load(bot_file_path)
+
+    if splitext(bot_file_path)[1] == PYTHON_EXT:
+
+        inject(code, PY_DO_TURN_STATEMENT, PY_GAME_OBJECT_WRAPPING, True)
+
+    elif splitext(bot_file_path)[1] == JAVA_EXT:
+        pass
+
+    elif splitext(bot_file_path)[1] == C_SHARP_EXT:
+        pass
+
 
 if __name__ == "__main__":
+
     import sys
-    manipulate(load(sys.argv[1]))
-    #save(manipulate(load(sys.argv[1])),sys.argv[2])
+
+    inject_adapter(sys.argv[1])
+    # save(manipulate(load(sys.argv[1])),sys.argv[2])
---
+++ bot_analyzer/game_wrapper.py
@@ -0,0 +1,7 @@
+class GameWrapper():
+    def __init__(self, game):
+        self.game = game
+
+        # TODO: Copy all methods from pythonRunner.py's Pirates class and change the body according to the following form:
+        # def same_method_name(self, arg1, arg2, etc...):
+        #    return self.game.same_method_name(arg1, arg2, etc...)
