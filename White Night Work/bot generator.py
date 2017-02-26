cb05e64371cdc852b021b780a90817510a5dffe6
roeyZemmel <roey.zemel@gmail.com>
2017-02-26 20:56:29 +0200

    added the bot_generator.py file.

--- White Night Work/bot generator.py
+++ White Night Work/bot generator.py
@@ -1,1 +1,17 @@
 __author__ = 'roeyz'
+import random as rdm
+commands_dict = {}
+id=0
+def generate_matrices():
+    """
+    a generator of random relevant matrices.
+    :return:
+    """
+    pass
+def mat2bot(dest_path="",matrix):
+    """
+
+    :param path:
+    :param matrix:
+    :return:
+    """
