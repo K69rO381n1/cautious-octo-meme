b296d0ab218fd73d73b50f0001e5a714187817dd
JonatanHadas <jonatan@hadaso.net>
2017-03-20 22:12:38 +0200

    Add connector between battle interface to bot analyzer. Implementation details will be finished a little later.
    A few functions from other modules are missing, will be finished when they exist.

---
+++ analyzer_connector.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 21:49:12 2017
+
+@author: Jonatan Hadas
+"""
+
+import battle_interface as b_int
+import bot_analyzer.analyzer as anal
+import os
+
+bots_path = os.path.join(b_int.kit_path, 'bots')
+
+def sparse_matrix_add_into(mat1, mat2):
+    """
+    Adds mat2 into mat1
+    """
+    for x in mat2:
+        if x not in mat1:
+            mat1[x] = dict(mat2[x])
+        else:
+            for y in mat2[x]:
+                if y in mat1[x]:
+                    mat1[x][y]+=mat2[x][y]
+                else:
+                    mat1[x][y] = mat2[x][y]
+            
+    
+
+def analyze_bot(name, map = ''):
+    matrix = {}    
+    
+    cur_bot_name = os.path.join(bots_path, name)
+    #inject code
+    new_bot_name = os.abspath('bots/current_experiment_bot.py')
+    matrix_name = os.abspath('bots/current_experiment_matrix.py')
+    #save file into new_bot_name
+    
+    for bot in b_int.bots():
+        #run
+        run(cur_bot_name, bot, map)
+        #load matrix from file
+        cur_matrix = None
+        
+        sparse_matrix_add_into(matrix, cur_matrix)
+    
+    return matrix
